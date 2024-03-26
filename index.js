ea58f9183c1014eb7ba4a084b0c12d8fce5305de
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-03-27 06:28:03 +0700

    wip: add check winning condition

--- index.js
+++ index.js
@@ -30,25 +30,91 @@ function Gameboard() {
     return board;
   }
 
-  function placeMark(row, column, player) {
+  function getBoardWithValues() {
+    return board.map(row => row.map(cell => cell.getValue()));
+  }
+
+  function placeMark(row, column, playerSymbol) {
     const currentSpot = board[row][column];
     if (currentSpot.getValue() != 0) {
       return false;
     } else {
-      currentSpot.markSpot(player);
+      currentSpot.markSpot(playerSymbol);
       return true;
     }
   }
 
+  function checkIfWon(playerSymbol) {
+    const flatBoardWithValues = getBoardWithValues().flat();
+
+    for (let rowIndex = 0; rowIndex < flatBoardWithValues.length; rowIndex += boardSize) {
+      const row = flatBoardWithValues.slice(rowIndex, rowIndex + boardSize);
+
+      // Check row
+      if (row.every(item => item === playerSymbol)) {
+        return true;
+      }
+
+      if (rowIndex = 0) {
+        // Check column
+        row.forEach((rowItem, index) => {
+          let column = [];
+
+          for (let columnIndex = index; columnIndex <= flatBoardWithValues.length - boardSize + index; columnIndex += boardSize) {
+            column.push(flatBoardWithValues[columnIndex]);
+          }
+        })
+      }
+    }
+
+    // const boardWithValues = getBoardWithValues();
+
+    // boardWithValues.forEach((row, rowIndex) => {
+    //   // Check row
+    //   if (row.every((column, columnIndex, array) => column === array[0])) return true;
+
+    //   if (rowIndex === 0) {
+    //     row.forEach((column, columnIndex, array) => {
+    //       let currentArray = []
+
+    //       if ((columnIndex === 0) || (columnIndex === boardSize - 1)) {
+    //         // Check diagonal
+    //         currentArray.push(column);
+
+    //         if (columnIndex === 0) {
+    //           for (let index = 1; index < boardSize; index++) {
+    //             currentArray.push(boardWithValues[index][index]);
+    //           }
+    //         } else {
+              
+    //         }
+  
+    //         if (currentArray.every((verticalItem, columnIndex, array) => verticalItem === array[0])) return true;
+
+    //         currentArray = []
+    //       }
+
+    //       // Check column
+    //       currentArray.push(column);
+
+    //       for (let index = 1; index < boardSize; index++) {
+    //         currentArray.push(boardWithValues[index][columnIndex]);
+    //       }
+
+    //       if (currentArray.every((verticalItem, columnIndex, array) => verticalItem === array[0])) return true;
+    //     });
+    //   }
+    // });
+  }
+
   function printBoard() {
-    const boardWithValues = board.map(row => row.map(cell => cell.getValue()));
-    
-    console.log(boardWithValues);
+    console.log(getBoardWithValues());
   }
 
   return {
     getBoard,
     placeMark,
+    checkIfWon,
     printBoard
   }
 }
